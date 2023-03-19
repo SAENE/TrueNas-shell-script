ea08d6c42c603181d8250a37c22d2ce9bae06d13
SAENE <50165443+SAENE@users.noreply.github.com>
2023-03-19 14:07:33 +0800

    修正部分错误，增加最大占用应用监测

--- cpu-freq-controller.sh
+++ cpu-freq-controller.sh
@@ -13,11 +13,12 @@ cpu_super_temp_plus_limit=90 #紧急温度
 
 ##频率设置
 cpu_normal_temp_limit_max_freq=`expr 3200 \* 1000` #正常温度频率
-cpu_normal_temp_limit_nomarl_freq=`expr 1300 \* 1000` #正常温度频率
+cpu_normal_temp_limit_nomarl_freq=`expr 1200 \* 1000` #正常温度频率
 cpu_super_temp_limit_max_freq=`expr 2400 \* 1000` #高温温度频率
 cpu_super_temp_limit_plus_max_freq=`expr 1400 \* 1000` #紧急温度频率
-cpu_temp_limit_min_freq=`expr 1200 \* 1000` #最低频率
-cpu_min_idle=35 #cpu最小占空比
+cpu_temp_limit_min_freq=`expr 1300 \* 1000` #最低频率
+cpu_min_idle=40 #cpu最小占空比
+app_use_max_cpu_limit=100
 
 ##客观变量获取命令
 fan_mode_get=`ipmitool raw 0x30 0x45 0x00`
@@ -28,6 +29,7 @@ cpu_temp_sys_get="echo $[$(cat /sys/class/thermal/thermal_zone0/temp)/1000]" #�
 cpu1_temp_ipmi_get=`ipmitool sdr | grep CPU1 | grep Temp | sed 's/[1-2]//' | sed 's/| ok//g' | sed 's/CPU Temp.*| //g' | sed 's/ degrees C//g'` #通过ipmi获取cpu温度
 cpu2_temp_ipmi_get=`ipmitool sdr | grep CPU2 | grep Temp | sed 's/[1-2]//' | sed 's/| ok//g' | sed 's/CPU Temp.*| //g' | sed 's/ degrees C//g'` #同上
 cpu_idle=`top -bcn 1 -w 200 | grep '%Cpu(s)' | sed 's/.*ni,//g' | sed 's/\..*id,.*//g' | awk -F'[" "%]+' '{print $2}' | sed 's/root//g' | sed -n '1p'` #cpu占空比获取
+app_use_max_cpu=`top -bcn 1 -w 200 | sed -n '8,20p' | sed 's/.*plexmediaserver.*//g' | sed 's/.*Emby.*//g' | sed 's/.*qemu.*//g' | awk -F'[" "%]+' '{print $10}' | sed 's/\..//g' | sed '/^\s*$/d' | sed -n '1p'`
 
 #检测命令是否安装
 if [ ! type cpufreq-set > /dev/null 2>&1 ] 
@@ -40,10 +42,10 @@ fi
 
 
 #根据温度调节cpu频率和风扇策略
-##正常温度频率调节&cpu省电
+##正常温度频率调节
 if [[  `${cpu_temp_sys_get}` -lt ${cpu_normal_temp_limit} || ${cpu1_temp_ipmi_get} -lt ${cpu_normal_temp_limit} && ${cpu2_temp_ipmi_get} -lt ${cpu_normal_temp_limit} ]]
 then
-    if [[ ${cpu_idle} -gt ${cpu_min_idle} && `${cpu_max_freq_get}` -gt ${cpu_normal_temp_limit_nomarl_freq} ]]
+    if [[ ${cpu_idle} -gt ${cpu_min_idle} && `${cpu_max_freq_get}` -gt ${cpu_normal_temp_limit_nomarl_freq} || ${app_use_max_cpu} -le ${app_use_max_cpu_limit} && `${cpu_max_freq_get}` -gt ${cpu_normal_temp_limit_nomarl_freq} ]]
     then
         echo "cpu温度正常，正在恢复设置"
         echo "cpu占用低，正在降低cpu性能"
@@ -59,7 +61,7 @@ then
             ipmitool raw 0x30 0x45 0x01 0x02
         fi
     else
-        if [[ `${cpu_max_freq_get}` -gt ${cpu_normal_temp_limit_nomarl_freq} ]]
+        if [[ ${cpu_idle} -le ${cpu_min_idle} && `${cpu_max_freq_get}` -lt ${cpu_normal_temp_limit_max_freq} || ${app_use_max_cpu} -gt ${app_use_max_cpu_limit} && `${cpu_max_freq_get}` -lt ${cpu_normal_temp_limit_max_freq} ]]
         then
             echo '有应用高占用，正在提高cpu性能'
             for((i=0;i<=39;i++));
@@ -120,6 +122,7 @@ else
 fi
 
 #打印系统信息
+echo "最大cpu占用应用${app_use_max_cpu}"
 echo "cpu空占比 ${cpu_idle}%"
 echo `${cpu_max_freq_get}`
 cat /proc/cpuinfo | grep MHz | tail -n1
