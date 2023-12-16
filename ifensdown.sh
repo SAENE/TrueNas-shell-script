49aa91db85e911ef0c33ab2a06e9a716da1a1451
SAENE <2944263786@qq.com>
2023-12-16 20:21:01 +0800

    增加动态获取网卡信息

--- ifensdown.sh
+++ ifensdown.sh
@@ -3,19 +3,18 @@ date
 for num_list in 1 2 3
 do
 ens_up_num=0
-ens_status=1
 echo "本次脚本第${num_list}个循环"
-ens12_status=`ip link | grep enp12s0 | grep -o DOWN`
-ens11_status=`ip link | grep enp11s0 | grep -o DOWN`
-ens8_status=`ip link | grep enp8s0 | grep -o DOWN`
-ens7_status=`ip link | grep enp7s0 | grep -o DOWN`
+# ens12_status=`ip link | grep enp12s0 | grep -o DOWN`
+# ens11_status=`ip link | grep enp11s0 | grep -o DOWN`
+# ens8_status=`ip link | grep enp8s0 | grep -o DOWN`
+# ens7_status=`ip link | grep enp7s0 | grep -o DOWN`
+ens_list=$(ip a | egrep -o 'enp[0-9]{1,2}s[0-9]')
 
-ens_status=(${ens12_status} ${ens11_status} ${ens8_status} ${ens7_status})
-
-for loop in 0 1 2 3
+for loop in ${ens_list}
 do
-	echo "${ens_status[loop]}"
-	if [ -z ${ens_status[loop]} ]
+	echo "${loop}"
+	ens_status=$(ip link | grep ${loop} | grep -o DOWN)
+	if [ -z ${ens_status} ]
 	then
 		echo "这个网卡已启动"
 		ens_up_num=1
@@ -27,10 +26,11 @@ done
 if [[ ${ens_up_num} -ne 1 ]]
 then
 	echo "所有网卡已掉线，正在拉起网卡"
-	ip link set enp12s0 up
-	ip link set enp11s0 up
-	ip link set enp8s0 up
-	ip link set enp7s0 up
+	for loop in ${ens_list}
+	do
+	    echo "正在拉起${loop}"
+	    ip link set ${loop} up
+	done
 else
 	echo "有张网卡已连接，无需拉起"
 fi
